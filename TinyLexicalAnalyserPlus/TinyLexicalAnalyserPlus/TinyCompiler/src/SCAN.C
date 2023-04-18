ca12d80a78ec9beb0cb33112d7483f73a9cbbdda
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-04-19 02:27:14 +0800

    Basic multi-line comment identification completed.
    Error check needs to be added.

--- TinyLexicalAnalyserPlus/TinyLexicalAnalyserPlus/TinyCompiler/src/SCAN.C
+++ TinyLexicalAnalyserPlus/TinyLexicalAnalyserPlus/TinyCompiler/src/SCAN.C
@@ -12,7 +12,9 @@
 /* states in scanner DFA */
 typedef enum
 {
-	START, INASSIGN, INCOMMENT, INNUM, INID, DONE, INCMULTICOMMENT, INFLOAT
+	START, INASSIGN, INCOMMENT, INNUM, INID, DONE, 
+	INMULTILINECOMMENT_1, INMULTILINECOMMENT_2, INMULTILINECOMMENT_3,
+	INFLOAT
 }
 StateType;
 
@@ -80,64 +82,6 @@ static TokenType reservedLookup(char* s)
 	return ID;
 }
 
-/* Probe whether continue to state 2 when in state 4 for multicomment scanning */
-static int ProbeBackTrack(int c)
-{
-	// save the spot
-	FILE* sourceBackup = malloc(sizeof(FILE));
-	char* lineBufBackup = malloc(BUFLEN);
-	if (!sourceBackup || !lineBufBackup)
-	{
-		printf("Out of memory!\n");
-		exit(1);
-	}
-	memcpy(sourceBackup, source, sizeof(FILE));
-	memcpy(lineBufBackup, lineBuf, BUFLEN);
-
-	int lineposBackup = linepos;
-	int bufsizeBackup = bufsize;
-	int linenoBackup = lineno;
-	int EOF_flag_Backup = EOF_flag;
-	long streamPos = ftell(source);
-
-	int firstChar = c, secondChar = getNextChar();
-	while (!EOF_flag)
-	{
-		if (firstChar == '*' && secondChar == '/')
-		{
-			// recover the spot
-			memcpy(source, sourceBackup, sizeof(FILE));
-			memcpy(lineBuf, lineBufBackup, BUFLEN);
-			free(sourceBackup);
-			free(lineBufBackup);
-
-			linepos = lineposBackup;
-			bufsize = bufsizeBackup;
-			lineno = linenoBackup;
-			EOF_flag = EOF_flag_Backup;
-
-			return FALSE;
-		}
-
-		firstChar = secondChar;
-		secondChar = getNextChar();
-	}
-
-	// recover the spot
-	memcpy(source, sourceBackup, sizeof(FILE));
-	memcpy(lineBuf, lineBufBackup, BUFLEN);
-	free(sourceBackup);
-	free(lineBufBackup);
-
-	linepos = lineposBackup;
-	bufsize = bufsizeBackup;
-	lineno = linenoBackup;
-	EOF_flag = EOF_flag_Backup;
-	fseek(source, streamPos, SEEK_SET);
-
-	return TRUE;
-}
-
 /****************************************/
 /* the primary function of the scanner  */
 /****************************************/
@@ -159,263 +103,174 @@ TokenType getToken(void)
 		save = TRUE;
 		switch (state)
 		{
-			case START:
-				if (isdigit(c))
-					state = INNUM;
-				else if (isalpha(c))
-					state = INID;
-				else if (c == ':')
-					state = INASSIGN;
-				else if ((c == ' ') || (c == '\t') || (c == '\n'))
-					save = FALSE;
-				else if (c == '{')
-				{
-					save = FALSE;
-					state = INCOMMENT;
-				}
-				else
-				{
-					state = DONE;
-					switch (c)
-					{
-						case EOF:
-							save = FALSE;
-							currentToken = ENDFILE;
-							break;
-						case '=':
-							currentToken = EQ;
-							break;
-						case '<':
-							currentToken = LT;
-							break;
-						case '+':
-							currentToken = PLUS;
-							break;
-						case '-':
-							currentToken = MINUS;
-							break;
-						case '*':
-							currentToken = TIMES;
-							break;
-						case '/':
-							if (linepos < bufsize && lineBuf[linepos] == '*')
-							{
-								state = INCMULTICOMMENT;
-								save = FALSE;
-							}
-							else currentToken = OVER;
-							break;
-						case '(':
-							currentToken = LPAREN;
-							break;
-						case ')':
-							currentToken = RPAREN;
-							break;
-						case ';':
-							currentToken = SEMI;
-							break;
-						default:
-							currentToken = ERROR;
-							break;
-					}
-				}
-				break;
-			case INCOMMENT:
+		case START:
+			if (isdigit(c))
+				state = INNUM;
+			else if (isalpha(c))
+				state = INID;
+			else if (c == ':')
+				state = INASSIGN;
+			else if ((c == ' ') || (c == '\t') || (c == '\n'))
 				save = FALSE;
-				if (c == EOF)
-				{
-					state = DONE;
-					currentToken = ENDFILE;
-				}
-				else if (c == '}') state = START;
-				break;
-			case INASSIGN:
+			else if (c == '{')
+			{
+				save = FALSE;
+				state = INCOMMENT;
+			}
+			else
+			{
 				state = DONE;
-				if (c == '=')
-					currentToken = ASSIGN;
-				else
-				{ /* backup in the input */
-					ungetNextChar();
+				switch (c)
+				{
+				case EOF:
 					save = FALSE;
-					currentToken = ERROR;
-				}
-				break;
-			case INNUM:
-				if (!isdigit(c))
-				{ 
-					if (c == '.')
+					currentToken = ENDFILE;
+					break;
+				case '=':
+					currentToken = EQ;
+					break;
+				case '<':
+					currentToken = LT;
+					break;
+				case '+':
+					currentToken = PLUS;
+					break;
+				case '-':
+					currentToken = MINUS;
+					break;
+				case '*':
+					currentToken = TIMES;
+					break;
+				case '/':
+					if (linepos < bufsize && lineBuf[linepos] == '*')
 					{
-						state = INFLOAT;
-					}
-					else
-					{
-						/* backup in the input */
-						ungetNextChar();
+						state = INMULTILINECOMMENT_1;
 						save = FALSE;
-						state = DONE;
-						currentToken = NUM;
 					}
+					else currentToken = OVER;
+					break;
+				case '(':
+					currentToken = LPAREN;
+					break;
+				case ')':
+					currentToken = RPAREN;
+					break;
+				case ';':
+					currentToken = SEMI;
+					break;
+				default:
+					currentToken = ERROR;
+					break;
 				}
-				break;
-			case INID:
-				if (!isalpha(c))
-				{ /* backup in the input */
+			}
+			break;
+		case INCOMMENT:
+			save = FALSE;
+			if (c == EOF)
+			{
+				state = DONE;
+				currentToken = ENDFILE;
+			}
+			else if (c == '}') state = START;
+			break;
+		case INASSIGN:
+			state = DONE;
+			if (c == '=')
+				currentToken = ASSIGN;
+			else
+			{ /* backup in the input */
+				ungetNextChar();
+				save = FALSE;
+				currentToken = ERROR;
+			}
+			break;
+		case INNUM:
+			if (!isdigit(c))
+			{
+				if (c == '.')
+				{
+					state = INFLOAT;
+				}
+				else
+				{
+					/* backup in the input */
 					ungetNextChar();
 					save = FALSE;
 					state = DONE;
-					currentToken = ID;
+					currentToken = NUM;
 				}
-				break;
-			case INCMULTICOMMENT:
+			}
+			break;
+		case INID:
+			if (!isalpha(c))
+			{ /* backup in the input */
+				ungetNextChar();
 				save = FALSE;
+				state = DONE;
+				currentToken = ID;
+			}
+			break;
+		case INMULTILINECOMMENT_1:
+			save = FALSE;
+			if (c == '*')
+			{
+				state = INMULTILINECOMMENT_2;
+			}
+			else if (c == EOF)
+			{
+				/* should never happen */
+				state = DONE;
+				currentToken = ENDFILE;
+			}
+			else
+			{
+				/* should never happen */ 
 				ungetNextChar();
-				int multicommentState = 1;
-
-				while (multicommentState == 1 || multicommentState == 2
-					|| multicommentState == 3 || multicommentState == 4)
-				{
-					c = getNextChar();
-					switch (multicommentState)
-					{
-						case 1:
-							switch (c)
-							{
-								case '*':
-									multicommentState = 2;
-									break;
-								default:
-									/* Should never happen */
-									fprintf(listing, "Scanner Bug: state= %d\n", state);
-									state = DONE;
-									currentToken = ERROR;
-									multicommentState = -1;
-									break;
-							}
-							break;
-						case 2:
-							switch (c)
-							{
-								case '*':
-									multicommentState = 3;
-									break;
-								default:
-									multicommentState = 2;
-									break;
-							}
-							break;
-						case 3:
-							switch (c)
-							{
-								case '*':
-									multicommentState = 3;
-									break;
-								case '/':
-									multicommentState = 4;
-									break;
-								default:
-									multicommentState = 2;
-									break;
-							}
-							break;
-						case 4:
-							/* Probe whether need to backtrack */
-							if (!ProbeBackTrack(c))
-							{
-								switch (c)
-								{
-								case '*':
-									multicommentState = 3;
-									break;
-								default:
-									multicommentState = 2;
-									break;
-								}
-							}
-							else
-							{
-								ungetNextChar();
-								state = START;
-								multicommentState = 5;
-							}
-							break;
-						default:
-							/* Should never happen */
-							fprintf(listing, "Scanner Bug: state= %d\n", state);
-							state = DONE;
-							currentToken = ERROR;
-							multicommentState = -1;
-							break;
-					}
-				}
-				break;
-			case INFLOAT:
-				ungetNextChar();
-				int inFloatState = 1;
-				while (inFloatState == 1 || inFloatState == 2)
-				{
-					c = getNextChar();
-					switch (inFloatState)
-					{
-					case 1:
-						if (c == '0' || c == '1' || c == '2' ||
-							c == '3' || c == '4' || c == '5' ||
-							c == '6' || c == '7' || c == '8' ||
-							c == '9')
-						{
-							inFloatState = 2;
-						}
-						else
-						{
-							/* Should never happen */
-							fprintf(listing, "Scanner Bug: state= %d\n", state);
-							state = DONE;
-							currentToken = ERROR;
-							inFloatState = -1;
-						}
-						break;
-					case 2:
-						if (c == '0' || c == '1' || c == '2' ||
-							c == '3' || c == '4' || c == '5' ||
-							c == '6' || c == '7' || c == '8' ||
-							c == '9')
-						{
-							inFloatState = 2;
-						}
-						else
-						{
-							ungetNextChar();
-							state = DONE;
-							currentToken = FLOATNUM;
-							inFloatState = 3;
-						}
-						break;
-					default:
-						/* Should never happen */
-						fprintf(listing, "Scanner Bug: state= %d\n", state);
-						state = DONE;
-						currentToken = ERROR;
-						inFloatState = -1;
-						break;
-					}
-					if ((save) && (tokenStringIndex <= MAXTOKENLEN))
-						tokenString[tokenStringIndex++] = (char)c;
-					if (state == DONE)
-					{
-						tokenString[tokenStringIndex] = '\0';
-						if (TraceScan) {
-							fprintf(listing, "\t%d: ", lineno);
-							printToken(currentToken, tokenString);
-						}
-						return currentToken;
-					}
-				}
-				break;
-			case DONE:
-			default: /* should never happen */
-				fprintf(listing, "Scanner Bug: state= %d\n", state);
 				state = DONE;
 				currentToken = ERROR;
-				break;
+			}
+			break;
+		case INMULTILINECOMMENT_2:
+			save == FALSE;
+			if (c == '*')
+			{
+				state = INMULTILINECOMMENT_3;
+			}
+			else if (c == EOF)
+			{
+				state = DONE;
+				currentToken = ENDFILE;
+			}
+			else
+			{
+				state = INMULTILINECOMMENT_2;
+			}
+			break;
+		case INMULTILINECOMMENT_3:
+			save = FALSE;
+			if (c == '/')
+			{
+				state = START;
+			}
+			else if (c == '*')
+			{
+				state = INMULTILINECOMMENT_3;
+			}
+			else if (c == EOF)
+			{
+				state = DONE;
+				currentToken = ENDFILE;
+			}
+			else
+			{
+				state = INMULTILINECOMMENT_2;
+			}
+			break;
+		default: /* should never happen */
+			fprintf(listing, "Scanner Bug: state= %d\n", state);
+			state = DONE;
+			currentToken = ERROR;
+			break;
 		}
 		if ((save) && (tokenStringIndex <= MAXTOKENLEN))
 			tokenString[tokenStringIndex++] = (char)c;
